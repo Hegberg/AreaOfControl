9863295bc47659746386e485302a4e2795b276c2
hegberg <hegberg@ualberta.ca>
2017-08-13 12:46:58 -0500

    Basic Networking

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Connect();
+    }
+
+    void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings("v1.0.0");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Random Failed");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnPhotonJoinFailed()
+    {
+        Debug.Log("Failed");
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        SpawnMyPlayer();
+    }
+
+    void SpawnMyPlayer()
+    {
+        PhotonNetwork.Instantiate("RedTile", new Vector3(0,0,0), Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	private void Move()
+	{
+		if (Input.GetKey("a"))
+		{
+			MoveLeft();
+		}
+		if (Input.GetKey("d"))
+		{
+			MoveRight();
+		}
+			
+
+	}
+
+	private void MoveLeft()
+	{
+		transform.Translate(new Vector3(-0.08f, 0, 0));
+		//rb2D.MovePosition (transform.position - new Vector3 (0.08f, rb2D.velocity.y/100));
+	}
+
+	private void MoveRight()
+	{
+		transform.Translate(new Vector3(0.08f, 0, 0));
+		//rb2D.MovePosition (transform.position + new Vector3 (0.08f, rb2D.velocity.y/100));
+	}
+}
